ab1b9c94c188f6017ee7cb94a0c71b8249d3c164
binarflow <clouds.overflow@gmail.com>
2022-12-17 22:18:53 -0500

    .

--- app/app.py
+++ app/app.py
@@ -42,8 +42,4 @@ def index():
     return res
 
 
-port = int(os.environ.get('PORT', 8080)) # runs on port 8080
 
-if __name__ == '__main__':
-    # we want the server to be multi-threaded
-    app.run(threaded=True, host='0.0.0.0', port=port)
