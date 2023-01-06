47dee9c45b9436527b0d43a05a876b8e3a9f1fc5
binarflow <clouds.overflow@gmail.com>
2023-01-05 22:13:17 -0500

    updated port

--- app.py
+++ app.py
@@ -46,5 +46,5 @@ port = int(os.environ.get('PORT', 8080)) # runs on port 8080
 
 if __name__ == '__main__':
 #     # we want the server to be multi-threaded
-	app.run(threaded=True, host='0.0.0.0', port=port)
+	app.run(threaded=True, host='0.0.0.0', port=8080)
 
